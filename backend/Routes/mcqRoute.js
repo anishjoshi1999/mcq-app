40b2a4cc082b578652c22ce5096a8814738f7483
Anish Joshi <anish.joshi@cedargate.com>
2024-06-30 15:00:56 +0545

    Updated Route and md file

--- backend/Routes/mcqRoute.js
+++ backend/Routes/mcqRoute.js
@@ -12,28 +12,28 @@ const {
   deleteSpecificMCQ,
 } = require("../Controllers/mcqControllers");
 
+// Route to fetch all topics with name and slug
+router.get("/", getAllTopics);
+
 // Route to upload MCQs from JSON file to database and associate with topic
 router.post("/upload", uploadMCQS);
 
-// Route to fetch all topics with name and slug
-router.get("/topics", getAllTopics);
-
 // Route to retrieve all MCQs based on topic slug
-router.get("/topics/:topicSlug", getAllMCQsByTopic);
+router.get("/:topicSlug", getAllMCQsByTopic);
 
 // Route to append MCQs to an existing topic
-router.post("/topics/:topicSlug", appendMCQsToTopic);
+router.post("/:topicSlug", appendMCQsToTopic);
 
 // Route to delete all MCQs based on topic slug
-router.delete("/topics/:topicSlug", deleteAllMCQsByTopic);
+router.delete("/:topicSlug", deleteAllMCQsByTopic);
 
 // Route to fetch specific MCQ based on topic slug and question slug
-router.get("/topics/:topicSlug/:questionSlug", getMCQByTopicAndSlug);
+router.get("/:topicSlug/:questionSlug", getMCQByTopicAndSlug);
 
 // Route to update a specific MCQ based on topic slug and question slug
-router.put("/topics/:topicSlug/:questionSlug", updateSpecificMCQ);
+router.put("/:topicSlug/:questionSlug", updateSpecificMCQ);
 
 // Route to delete a specific MCQ based on topic slug and question slug
-router.delete("/topics/:topicSlug/:questionSlug", deleteSpecificMCQ);
+router.delete("/:topicSlug/:questionSlug", deleteSpecificMCQ);
 
 module.exports = router;
